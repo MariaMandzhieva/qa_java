59f40aa7e201743b01e1bc4ee7ce8567cc8bd7aa
Maria Mandzhieva <manmariia@yandex.ru>
2024-06-11 19:29:46 +0300

    commit edit LionParamsHasManeTest

--- src/test/java/com/example/LionParamsHasManeTest.java
+++ src/test/java/com/example/LionParamsHasManeTest.java
@@ -1,11 +1,8 @@
 package com.example;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.assertEquals;
 
